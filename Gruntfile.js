66c9c0d77bdef8bae2b2e8a3049938aede0383d3
leemicw <michael@michaelware.net>
2013-07-01 16:05:23 -0500

    Adding copy command to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,28 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
         src: 'Source/jquery.ImageMapResize.js',
-        dest: 'Build/jquery.ImageMapResize.min.js'
+        dest: 'Build/jquery.ImageMapResize-<%= pkg.version %>.min.js'
       }
-    }
+    },
+	copy: {
+		main:{
+			src: 'Source/jquery.ImageMapResize.js',
+			dest: 'Build/jquery.ImageMapResize-<%= pkg.version %>.js'
+		}
+	}
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  // Load the plugin that provides the copy task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'copy']);
 
 };
